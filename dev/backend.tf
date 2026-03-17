85b5e7b85320338561fef0e7af593aec78417ae0
Dilmin P.R. <130033091+Rash499@users.noreply.github.com>
2026-03-17 22:48:56 +0530

    Add access key to backend configuration
    Added access key for Azure storage backend.

--- dev/backend.tf
+++ dev/backend.tf
@@ -4,6 +4,9 @@ terraform {
     storage_account_name = "tfdevbackend2024piyush"
     container_name      = "tfstate"
     key                 = "dev.tfstate"
+
+    access_key = "SwL+JGwkAAA2kXTQk+g4phDLRzsbiKX4xqgp5V91cEmaiZw6grzkv5u0nfzeqyjGIsIJtR9ASTYu+AStGHgC4A=="
+
   }
 }
 
