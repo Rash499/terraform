ead8b31f1f3636179659c7c6666e598e43a88592
Rash499 <rashmikadilmin499@gmail.com>
2026-03-17 23:34:00 +0530

    remove the azuread auth

--- dev/backend.tf
+++ dev/backend.tf
@@ -4,7 +4,7 @@ terraform {
     storage_account_name = "tfdevbackend2026rash"
     container_name      = "tfstate"
     key                 = "dev.tfstate"
-
+    use_azuread_auth = false
 
   }
 }
--- staging/backend.tf
+++ staging/backend.tf
@@ -4,7 +4,7 @@ terraform {
     storage_account_name = "tfstagebackend2026rash"
     container_name      = "tfstate"
     key                 = "stage.tfstate"
-
+    use_azuread_auth = false
   }
 }
 
