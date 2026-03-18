9f640238e5b90e76957112a0151d9ae2e9428807
Rash499 <rashmikadilmin499@gmail.com>
2026-03-18 11:02:16 +0530

    ssh key changed to terraform generate

--- modules/aks/output.tf
+++ modules/aks/output.tf
@@ -2,3 +2,8 @@ output "config" {
     value = azurerm_kubernetes_cluster.aks-cluster.kube_config_raw
   
 }
+
+output "ssh_private_key" {
+  value     = tls_private_key.ssh.private_key_pem
+  sensitive = true
+}
